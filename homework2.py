ff1ff122bf2dbd93c33c10b88db636e89047f0cc
90208 <70868383+90208@users.noreply.github.com>
2020-09-21 08:43:40 +0800

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 08:03:21 2020
+
+@author: cis-user
+"""
+
+# string='I LOVE YOU'
+# s1=string[:1]
+# s2=string[2:6]
+# s3=string[7:]
+# print(s1)
+# print(s2)
+# print(s3)
+
+# string='I LOVE YOU'
+# s=string[::-1]
+# print(s)
+
+# string='I LOVE YOU'
+# s=string+'so'*100
+# t='much'
+# print(s,end='')
+# print(t)
+
+# string='I LOVE YOU'
+# import random as r
+# a=r.randint(1,100)
+# b='so'
+# c=b*a
+# d='much'
+# print(string,c,d)
