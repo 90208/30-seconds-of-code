61ae329d77e1e389fef65ab95d6a1508cab5bd8e
90208 <70868383+90208@users.noreply.github.com>
2020-09-28 10:10:26 +0800

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 08:12:43 2020
+
+@author: cis-user
+"""
+
+# score=['小徐',5,9,6,8,7,10,6]
+# s=score[1:]
+# a=max(s)
+# print(a)
+
+# score=['小徐',5,9,6,8,7,10,6]
+# s=score[1:]
+# a=min(s)
+# print(a)
+
+# score=['wang',5,9,6,8,7,10,6]
+# s=score[1:]
+# y= sorted(s,reverse=True)
+# a,b,c,d,e,f,g=y
+# print(y[0:3])
+
+# score=['wang',5,9,6,8,7,10,6]
+# s=score[1:]
+# y= sorted(s,reverse=True)
+# a,b,c,d,e,f,g=y
+# print(y[4:7])
+
+# score=['小徐',5,9,6,8,7,10,6]
+# s=score[1:]
+# x=sum(s)/len(s)
+# print(x)
+
+
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print('{}x{}={}\t'.format(j,i,f'{i*j:>2}'),end='')
+#     print()
+
+# string='I LOVE YOU'
+# import random as r
+# a=r.randint(1,1000)
+# b=' so'*a+' much'
+# print(string,b)
