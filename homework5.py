77134b8acbff7ddc6c66425c1ff6adc7c9315fe9
90208 <70868383+90208@users.noreply.github.com>
2020-10-12 08:18:35 +0800

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 08:16:33 2020
+
+@author: cis-user
+"""
+
+# a='Simon Peter John'
+# b=a.split()
+# c='*'
+# for i in range(0,3):
+#     print(b[i][0],end='')
+#     print(c*(len(b[i])-1),end=' ')
+
+# a=['Company 1','Company 2','Company 3']
+# b=[]
+# for i in range(0,3):
+#     b.append(a[i].replace(' ','_'))
+# print(b,end=' ')
+
+# a=[1,2,3,4,5,6]
+# b=[]
+# c='$'
+# for i in range(1,7):
+#     b.append(str(i)+c)
+# print(b,end='')
+
+# a=['1$', '2$', '3$', '4$', '5$', '6$']
+# b=[]
+# for i in range(0,6):
+#       b.append(int(a[i].replace('$','')))
+# print(b)
+
+# a=[1,2,3,4]
+# b=[5,6,7,8]
+# print(list(zip(a,b)))
+
+# s= "I love you and you love him and who loves who"
+# b=s.split()
+# keys=set(b)
+# values=[0 for i in keys]
+# d={key:value for key,value in zip(keys,values)}
+# for i in b:    
+#     d[i]+=1
+# print(d)
