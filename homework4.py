65a7474bc1ff2b6d0b68de1b598db7163c92a3b1
90208 <70868383+90208@users.noreply.github.com>
2020-10-12 08:29:45 +0800

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 10:34:36 2020
+
+@author: cis-user
+"""
+# list11=[1,2,3,4,5]
+# list12=[6,5,4,3,2]
+# for i in range(0,5):
+#         if list11[i]>list12[i]:
+#             print(list11[i],end='')
+#         else:
+#             print(list12[i],end='')
+            
+    
+# list1 = [1,2,3,4,5]
+# list2 = [6,5,4,3,2]
+# list3 = [False,False,True,False,True]
+# for i in range(0,5):
+#         if list3[i]==False:
+#             print(list2[i],end='')
+#         else:
+#             print(list1[i],end='')
+            
+            
+    
+        
+
+
